6e16128e437dfdd45458a704279319f956253eb8
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-01-30 17:21:32 +0100

    betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     binary_tree_t *new_n;
 
     new_n = malloc(sizeof(binary_tree_t));
-    if(new_n == NULL)
+    if (new_n == NULL)
         return (NULL);
 
     new_n->n = value;
