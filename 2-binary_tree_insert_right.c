6b2854a95beb25e16226e2242d10c60fadac4a82
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-01-31 02:34:09 +0100

    ++

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,20 +2,21 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t new_n;
+	binary_tree_t *new_n;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    new_n = binary_tree_node(parent, value);
-    if (new_n == NULL)
-        return(NULL);
+	new_n = binary_tree_node(parent, value);
+	if (new_n == NULL)
+		return (NULL);
 
-    if (parent->right != NULL){
-        new_n->right = parent->right;
-        parent->right->parent = new_n;
-    }
-    parent->right = new_n;
+	if (parent->right != NULL)
+	{
+		new_n->right = parent->right;
+		parent->right->parent = new_n;
+	}
+	parent->right = new_n;
 
-    return (new_n);
+	return (new_n);
 }
